b63b5b01191fed0acc7704401f5427d33eb042ad
WYL <mmotashibana@gmail.com>
2025-05-14 17:22:46 +0900

    'fix-error'

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -41,6 +41,6 @@ class AuthController extends Controller
     {
         Auth::logout();
         $request->session()->regenerate();
-        return redirect()->route('auth.login');
+        return redirect()->route('login');
     }
 }
--- resources/js/Pages/Dashboard/Quiz/Index.vue
+++ resources/js/Pages/Dashboard/Quiz/Index.vue
@@ -25,16 +25,16 @@
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-6 py-3 min-w-[200px]">
                             Title
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-6 py-3 min-w-[200px]">
                             Created By
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-6 py-3 min-w-[200px]">
                             Status
                         </th>
-                        <th scope="col" class="px-6 py-3">
+                        <th scope="col" class="px-6 py-3 min-w-[200px]">
                             Action
                         </th>
                     </tr>
