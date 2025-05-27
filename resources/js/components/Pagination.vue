2e8343d8ffb216d798e521563ffadcb27ea9742a
WYL <mmotashibana@gmail.com>
2025-05-27 11:05:08 +0900

    'fix'

--- resources/js/components/Pagination.vue
+++ resources/js/components/Pagination.vue
@@ -45,7 +45,7 @@
                 <Link
                     preserve-scroll
                     :href="nextPageUrl || '#'"
-                    :class="{ 'pointer-events-none': prevPageUrl === null }"
+                    :class="{ 'pointer-events-none': nextPageUrl === null }"
                     class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                     <span class="sr-only">Next</span>
